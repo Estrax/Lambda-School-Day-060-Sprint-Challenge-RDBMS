90839c3fc4c6f9ecc7ce23c79dc3be07de793fc4
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-29 12:13:24 -0400

    added routes to manage contexts

---
+++ src/routes/contexts.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const db = require('../database/helpers/helpers');
+
+router.route('/')
+    .get(async (req, res) => {
+        await db.getContexts()
+            .then(contexts => res.status(200).json(contexts))
+            .catch(err => res.status(500).json({ error: "Contexts could not be retrieved."}));
+    })
+    .post(async (req, res) => {
+        await db.addContext(req.body)
+            .then(context => res.status(201).json(context))
+            .catch(err => res.status(500).json({ error: "Context could not be added."}));
+    });
+
+router.route('/:id')
+    .get(async (req, res) => {
+        await db.getContextById(req.params.id)
+            .then(context => res.status(200).json(context))
+            .catch(err => res.status(500).json({ error: "Context could not be retrieved."}));
+    })
+    .put(async (req, res) => {
+        await db.updateContext(req.params.id, req.body)
+            .then(
+                context => context.length === 0
+                ? res.status(404).json({ message: "The context with the specified ID does not exist." })
+                : res.status(200).json(context)
+            )
+            .catch(err => res.status(500).json({ error: "Context could not be updated."}));
+    })
+    .delete(async (req, res) => {
+        await db.removeContext(req.params.id)
+            .then(
+                context => context.length === 0
+                ? res.status(404).json({ message: "The context with the specified ID does not exist." })
+                : res.status(200).json(context))
+            .catch(err => res.status(500).json({ error: "Context could not be deleted." }));
+    });
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const projects = require('./projects');
 const actions = require('./actions');
+const contexts = require('./contexts');
 
 router.use('/projects', projects);
 router.use('/actions', actions);
+router.use('/contexts', contexts);
 
 module.exports = router;
