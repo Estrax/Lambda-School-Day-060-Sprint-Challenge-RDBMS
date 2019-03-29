26a4b9c98da8c15d938a8c217b153006d2ac79da
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-29 13:58:08 -0400

    updated foreign keys

--- src/database/helpers/db.js
+++ src/database/helpers/db.js
@@ -2,6 +2,6 @@ module.exports = {
     dropTable: (name) => knex => knex.schema.dropTableIfExists(name),    
     fkey: (table, name, ref) => {
         table.integer(name).unsigned().notNullable();
-        table.foreign(name).references('id').on(ref);
+        table.foreign(name).references('id').on(ref).onDelete('CASCADE');
     },
 }
