739894fe86f163a7f8a168883a2ca53ac0a994bb
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-29 11:25:06 -0400

    fixed typos in db seeds

--- src/database/seeds/projects.js
+++ src/database/seeds/projects.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
   return knex('projects').truncate()
     .then(function () {
-      return knex('project').insert([
+      return knex('projects').insert([
         {id: 1, name: 'First project', description: 'Description of first project', completed: false},
-        {id: 1, name: 'Second project', description: 'Description of second project', completed: false},
-        {id: 1, name: 'Third project', description: 'Description of third project', completed: false},
+        {id: 2, name: 'Second project', description: 'Description of second project', completed: false},
+        {id: 3, name: 'Third project', description: 'Description of third project', completed: false},
       ]);
     });
 };
