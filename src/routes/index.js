df27d9545b4b547c8d4b9048e025a792a005c618
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-29 11:29:55 -0400

    added default routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const projects = require('./projects');
+const actions = require('./actions');
+
+router.use('/projects', projects);
+router.use('/actions', actions);
 
 module.exports = router;
